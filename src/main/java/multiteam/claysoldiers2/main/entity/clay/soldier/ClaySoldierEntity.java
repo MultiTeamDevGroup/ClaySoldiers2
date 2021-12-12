4f77cf0c64f2b2c90136e4c2642839f35d65c525
LazTheArtist <kingofcreepersyt@gmail.com>
2021-12-12 23:06:13 +0100

    AI (random wandering; attacking non-similars [=Racism] )

--- src/main/java/multiteam/claysoldiers2/main/entity/clay/soldier/ClaySoldierEntity.java
+++ src/main/java/multiteam/claysoldiers2/main/entity/clay/soldier/ClaySoldierEntity.java
@@ -1,6 +1,5 @@
 package multiteam.claysoldiers2.main.entity.clay.soldier;
 
-import multiteam.claysoldiers2.main.item.ModItems;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
@@ -10,8 +9,14 @@ import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
+import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
+import net.minecraft.world.entity.ai.goal.WaterAvoidingRandomStrollGoal;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.entity.animal.Chicken;
+import net.minecraft.world.entity.animal.Rabbit;
 import net.minecraft.world.entity.item.ItemEntity;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.ServerLevelAccessor;
@@ -25,6 +30,7 @@ import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
 
 import javax.annotation.Nullable;
+import java.util.function.Predicate;
 
 public class ClaySoldierEntity extends PathfinderMob implements IAnimatable {
 
@@ -98,5 +104,27 @@ public class ClaySoldierEntity extends PathfinderMob implements IAnimatable {
     @Override
     public SpawnGroupData finalizeSpawn(ServerLevelAccessor p_21434_, DifficultyInstance p_21435_, MobSpawnType p_21436_, @Nullable SpawnGroupData p_21437_, @Nullable CompoundTag p_21438_) {
         return super.finalizeSpawn(p_21434_, p_21435_, p_21436_, p_21437_, p_21438_);
+    }
+
+    public boolean isSoldierMatching(ClaySoldierEntity soldierComparedWith){
+        if(this.getVariant() == soldierComparedWith.getVariant()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    @Override
+    protected void registerGoals() {
+        this.goalSelector.addGoal(0, new WaterAvoidingRandomStrollGoal(this, 1.0D));
+        this.goalSelector.addGoal(1, new RandomLookAroundGoal(this));
+        this.goalSelector.addGoal(2, new MeleeAttackGoal(this, 2, true));
+        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, ClaySoldierEntity.class, 4, true, false, (targetEntity) -> {
+            if(targetEntity instanceof ClaySoldierEntity){
+                ClaySoldierEntity targetedSoldier = (ClaySoldierEntity) targetEntity;
+                return !targetedSoldier.isSoldierMatching(this);
+            }
+            return false;
+        }));
     }
 }
